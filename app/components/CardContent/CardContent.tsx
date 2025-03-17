6c86b9df1dd79feb85b0cf5bbc366cfd4774de38
Omar Rendon <omarcr.96@gmail.com>
2025-03-17 02:56:30 -0600

    ADD: CardContent componet

---
+++ app/components/CardContent/CardContent.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+import { motion } from "motion/react";
+import "./CardContent.css";
+
+interface ICardProps {
+  title: string;
+  icon?: React.ReactNode;
+}
+export const CardContent: React.FC<ICardProps> = ({ icon, title }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        scale: { type: "spring", visualDuration: 0.8, bounce: 0.5 },
+      }}
+    >
+      <div
+        className="card bg-white border-1 border-gray-300 shadow-lg rounded-xl p-6 transition-all duration-800 ease-in-out
+                    group-hover:scale-75 hover:scale-110 hover:col-span-1 hover:border-blue-500 hover:min-w-2xs flex flex-col items-center justify-center hover:cursor-pointer "
+      >
+        <span>{icon}</span>
+        <h2 className="font-medium text-center mt-6">{title}</h2>
+      </div>
+    </motion.div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,96 @@
-import Image from "next/image";
+import { CardContent } from "./components/CardContent/CardContent";
+import { Calculator } from "lucide-react";
+import { CirclePercent } from "lucide-react";
+import { BadgeDollarSign } from "lucide-react";
+import { Receipt } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  min-h-screen p-8 pb-15 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <header>
+          <h1>
+            tu <span>Conta</span>
+          </h1>
+          <p>Tu Contador en Línea</p>
+          <p>
+            <strong>Nuevo servicio!</strong>
+          </p>
+          <p>Encárgate de tu negocio, nosotros de tu contabilidad.</p>
+        </header>
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-8xl w-full group ">
+            <CardContent
+              title="Hacemos tu contabilidad mensual"
+              icon={<Calculator size={"82px"} color="#3856DC" />}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <CardContent
+              title="Calculamos tus impuestos"
+              icon={<CirclePercent size={"82px"} color="#3856DC" />}
+            />
+            <CardContent
+              title="Presentamos tus declaraciones SAT"
+              icon={<BadgeDollarSign size={"82px"} color="#3856DC" />}
+            />
+            <CardContent
+              title="Cálculo, timbrado y énvio de nómina"
+              icon={<Receipt size={"82px"} color="#3856DC" />}
+            />
+          </section>
+
+          <section>
+            <video controls>
+              {/* <source src="video.mp4" type="video/mp4"> */}
+              Tu navegador no soporta el video.
+            </video>
+          </section>
+
+          <section>
+            <h2>Beneficios</h2>
+            {/* <ul>
+              <li>
+                <strong>Automático:</strong> Ahorra tiempo al eliminar la tarea
+                de enviar facturas a tu contador. ¡El proceso es automático!
+              </li>
+              <li>
+                <strong>Servicio Oportuno:</strong> Comunicación más fácil y
+                efectiva con tu contador personal.
+              </li>
+              <li>
+                <strong>Simplicidad:</strong> Información contable en tiempo
+                real.
+              </li>
+              <li>
+                <strong>Confiable:</strong> Cálculos hechos por uno de nuestros
+                contadores expertos asignado especialmente a tu negocio.
+              </li>
+            </ul> */}
+          </section>
+          <section>
+            <h2>¿Cómo funciona?</h2>
+            <ol>
+              <li>
+                <strong>Paso 1:</strong> Agrega tu FIEL
+              </li>
+              <li>
+                <strong>Paso 2:</strong> Sincroniza tus cuentas bancarias
+              </li>
+              <li>
+                <strong>Paso 3:</strong> Recibe mensualmente tu declaración de
+                impuestos
+              </li>
+            </ol>
+          </section>
+        </main>
+        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+          <p>
+            ¿Quieres cambiar a tu Conta? Nos encargamos de llevar a cabo la
+            transición de toda tu contabilidad hasta el día de hoy.
+          </p>
+          <button>Ver Paquetes</button>
+          <button>Agenda una cita con un contador experto</button>
+        </footer>
+      </div>
+    </>
   );
 }
